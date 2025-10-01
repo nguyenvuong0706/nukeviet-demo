bf0bf82644f1e11e9c5d19b2616c5b4fef8fba19
nguyenvuong0706 <21012530@st.phenikaa-uni.edu.vn>
2025-10-01 15:27:32 +0700

    Update code đã chỉnh sửa

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2025 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+$db_config['dbhost'] = '127.0.0.1';
+$db_config['dbport'] = '';
+$db_config['dbname'] = 'pet_heath';
+$db_config['dbsystem'] = 'pet_heath';
+$db_config['dbuname'] = 'root';
+$db_config['dbpass'] = '';
+$db_config['dbtype'] = 'mysql';
+$db_config['collation'] = 'utf8mb4_unicode_ci';
+$db_config['charset'] = 'utf8mb4';
+$db_config['persistent'] = false;
+$db_config['prefix'] = 'nv4';
+
+$global_config['site_domain'] = '';
+$global_config['name_show'] = 0;
+$global_config['idsite'] = 0;
+$global_config['sitekey'] = 'f2a76d58b147ebbef15e5be8c63a345f';// Do not change sitekey!
+$global_config['hashprefix'] = '{SSHA512}';
+$global_config['cached'] = 'files';
+$global_config['session_handler'] = 'files';
+$global_config['extension_setup'] = 3; // 0: No, 1: Upload, 2: NukeViet Store, 3: Upload + NukeViet Store
+$global_config['nat_ports'] = [];
+// Readmore: https://wiki.nukeviet.vn/nukeviet4:advanced_setting:file_config
